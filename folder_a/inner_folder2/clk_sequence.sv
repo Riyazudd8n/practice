fa8b048a7e578a0b9a7ab6e001c02ddcd5cc1830
Riyazudd8n <riyazudd8n@gmail.com>
2023-06-08 16:51:42 +0530

    practice pro pro

--- folder_a/inner_folder2/clk_sequence.sv
+++ folder_a/inner_folder2/clk_sequence.sv
@@ -13,3 +13,18 @@ class clk_sequence extends uvm_sequence#(clk_transaction);
                 finish_item(clk_trans);
         endtask: body
 endclass: clk_sequence
+class clk_sequence extends uvm_sequence#(clk_transaction);
+        `uvm_object_utils(clk_sequence)
+
+        function new(string name = "");
+                super.new(name);
+        endfunction: new
+
+        task body();
+                clk_transaction clk_trans;
+                clk_trans = clk_transaction::type_id::create(.name("clk_trans"), .contxt(get_full_name()));
+                start_item(clk_trans);
+                assert(clk_trans.randomize());
+                finish_item(clk_trans);
+        endtask: body
+endclass: clk_sequence
